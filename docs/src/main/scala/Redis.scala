bc23dc743829e2053ab2ea4bda84066c5db4a69f
Pritam Kadam <phkadam2008@gmail.com>
2020-06-05 23:01:41 +0530

    asd

--- docs/src/main/scala/Redis.scala
+++ docs/src/main/scala/Redis.scala
@@ -1,31 +1,8 @@
 import scala.util.control.NonFatal
 import scala.util.Try
 object Redis extends App {
-  private val redisServer = "redis-server"
 
-  def isInstalled(cmd: String): Try[Boolean]  = Try { new ProcessBuilder("bash", "command", "-v", cmd).start().waitFor() == 0 }
-  def isInstalled0(cmd: String): Try[Boolean] = Try { new ProcessBuilder("sh", "command", "-v", cmd).start().waitFor() == 0 }
-  def isInstalled1(cmd: String): Try[Boolean] = Try { Runtime.getRuntime.exec(Array("command", "-v", cmd)).waitFor() == 0 }
-  def isInstalled2(cmd: String): Try[Boolean] = Try {
-    Runtime.getRuntime.exec(Array("bash", "command", "-v", cmd)).waitFor() == 0
-  }
-  def isInstalled3(cmd: String): Try[Boolean] = Try { Runtime.getRuntime.exec(Array("sh", "command", "-v", cmd)).waitFor() == 0 }
-  def isInstalled4(cmd: String): Try[Boolean] = Try { Runtime.getRuntime.exec(Array("sh", "hash", cmd)).waitFor() == 0 }
-  def isInstalled5(cmd: String): Try[Boolean] = Try { Runtime.getRuntime.exec(Array("hash", cmd)).waitFor() == 0 }
+  def isInstalled(cmd: String*): Try[Boolean] = Try { new ProcessBuilder(cmd: _*).start().waitFor() == 0 }
 
-  println(isInstalled(redisServer))
-  println(isInstalled0(redisServer))
-  println(isInstalled1(redisServer))
-  println(isInstalled2(redisServer))
-  println(isInstalled3(redisServer))
-  println(isInstalled4(redisServer))
-  println(isInstalled5(redisServer))
-
-  println(isInstalled("ls"))
-  println(isInstalled0("ls"))
-  println(isInstalled1("ls"))
-  println(isInstalled2("ls"))
-  println(isInstalled3("ls"))
-  println(isInstalled4("ls"))
-  println(isInstalled5("ls"))
+  println(isInstalled("redis-server", "-v"))
 }
