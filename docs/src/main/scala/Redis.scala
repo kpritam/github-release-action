bbb2cdbca97c65014bd32ec2c16dbec7e4e937b6
Pritam Kadam <phkadam2008@gmail.com>
2020-06-05 21:21:55 +0530

    a

--- docs/src/main/scala/Redis.scala
+++ docs/src/main/scala/Redis.scala
@@ -1,14 +1,19 @@
+import scala.util.control.NonFatal
+import scala.util.Try
 object Redis extends App {
   private val redisServer = "redis-server"
 
-  def isInstalled(cmd: String): Boolean = new ProcessBuilder("type", cmd).start().waitFor() == 0
-
-  // todo: add version check
-  def server: String = {
-    if (isInstalled(redisServer)) redisServer
-    else throw new RuntimeException(s"$redisServer is not installed")
+  def isInstalled(cmd: String): Try[Boolean]  = Try { new ProcessBuilder("bash", "command", "-v", cmd).start().waitFor() == 0 }
+  def isInstalled0(cmd: String): Try[Boolean] = Try { new ProcessBuilder("sh", "command", "-v", cmd).start().waitFor() == 0 }
+  def isInstalled1(cmd: String): Try[Boolean] = Try { Runtime.getRuntime.exec(Array("command", "-v", cmd)).waitFor() == 0 }
+  def isInstalled2(cmd: String): Try[Boolean] = Try {
+    Runtime.getRuntime.exec(Array("bash", "command", "-v", cmd)).waitFor() == 0
   }
+  def isInstalled3(cmd: String): Try[Boolean] = Try { Runtime.getRuntime.exec(Array("sh", "command", "-v", cmd)).waitFor() == 0 }
 
   println(isInstalled(redisServer))
-  println(server)
+  println(isInstalled0(redisServer))
+  println(isInstalled1(redisServer))
+  println(isInstalled2(redisServer))
+  println(isInstalled3(redisServer))
 }
